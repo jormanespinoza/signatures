e4602763bafed5fe4bb309b30e43d385c18af652
Jorman <jespinoza@glamit.com.ar>
2021-08-30 01:07:39 -0300

    feature: Signatures filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,46 @@
 const searchInput = document.getElementById('search')
 const trTags = document.querySelectorAll('tbody tr')
 const h1 = document.querySelector('h1')
-
-const search = () => {
-  const searchInputValue = searchInput.value.toLowerCase()
-
+const getTrTagsToShow = () => {
+  const searchInputValues = searchInput.value.toLowerCase().split(' ')
+  const matches = []
+  let counter = 0
+  let trTagsToShow
   trTags.forEach((tr) => {
     const tdTags = [...tr.children]
     tdTags.pop()
-    let tdValues
-
     tdTags.forEach((td) => {
-      tdValues += ' ' + td.textContent || td.innerText
+      searchInputValues.forEach((searchInputValue) => {
+        const tdValue = td.textContent || td.innerText
+        if (!tdValue.toLowerCase().includes(searchInputValue.toLowerCase())) {
+          tr.style.display = 'none'
+        } else {
+          if (matches[counter]) {
+            matches[counter].hits += 1
+          } else {
+            matches[counter] = {
+              hits: 1,
+              tr
+            }
+          }
+        }
+      })
     })
 
-    tr.style.display = tdValues.toLowerCase().includes(searchInputValue) ? '' : 'none'
+    counter++
+    const maxHits = matches.reduce((acc, match) => acc > match.hits ? acc : match.hits, 0)
+
+    trTagsToShow = matches
+      .filter(match => match.hits === maxHits)
+      .map(match => match.tr)
   })
+
+  return trTagsToShow
+}
+const search = () => {
+  trTags.forEach(trTag => trTag.style.display = '')
+  if (searchInput.value.trim() === '') return
+  getTrTagsToShow().forEach(trToShow => trToShow.style.display = '')
 }
 
 searchInput.addEventListener(('keyup'), search)
